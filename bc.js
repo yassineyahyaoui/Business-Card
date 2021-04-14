e7f99f17adb1513aa5e45a72bd17888a4b8a8ba1
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2021-04-14 19:28:09 +0200

    transform property problem

---
+++ bc.js
@@ -0,0 +1,7 @@
+function rotate() {
+    document.getElementById("face").style.transform = "rotateY( 180deg) translate(0%, 50%)";
+}
+
+function derotate() {
+    document.getElementById("face").style.transform = "translate(-50%, -50%)";
+}
