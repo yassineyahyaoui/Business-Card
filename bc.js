63e7fea4e0ce472b52291eeb75e83a40c0e9c103
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2021-04-15 04:00:24 +0200

    fixing the hover and z-index, now i have backface-visibility problem

--- bc.js
+++
@@ -1,7 +0,0 @@
-function rotate() {
-    document.getElementById("face").style.transform = "rotateY( 180deg) translate(0%, 50%)";
-}
-
-function derotate() {
-    document.getElementById("face").style.transform = "translate(-50%, -50%)";
-}
